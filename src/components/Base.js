56b8d2fc71324a79e7e0fd04c72c6b6ec657386f
budokans <68995729+budokans@users.noreply.github.com>
2021-05-05 21:06:02 +1200

    whileHover animations on list items

--- src/components/Base.js
+++ src/components/Base.js
@@ -17,9 +17,14 @@ const Base = ({ addBase, pizza }) => {
         {bases.map((base) => {
           let spanClass = pizza.base === base ? "active" : "";
           return (
-            <li key={base} onClick={() => addBase(base)}>
+            <motion.li
+              key={base}
+              onClick={() => addBase(base)}
+              whileHover={{ scale: 1.3, color: "#f8e112", originX: 0 }}
+              transition={{ type: "spring", stiffness: 120 }}
+            >
               <span className={spanClass}>{base}</span>
-            </li>
+            </motion.li>
           );
         })}
       </ul>
