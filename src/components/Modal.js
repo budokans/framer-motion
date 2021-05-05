10c2473e066411047ded8c6c7f1a1d2127f22d10
budokans <68995729+budokans@users.noreply.github.com>
2021-05-06 01:11:48 +1200

    Create and style modal body

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -15,12 +15,21 @@ export default function Modal({ showModal, setShowModal }) {
   return (
     <AnimatePresence exitBeforeEnter>
       {showModal && (
-        <motion.div
-          className="backdrop"
-          variants={backdropVariants}
-          initial="hidden"
-          animate="visible"
-        ></motion.div>
+        <>
+          <motion.div
+            className="backdrop"
+            variants={backdropVariants}
+            initial="hidden"
+            animate="visible"
+          ></motion.div>
+
+          <motion.div className="modal">
+            <p>Want to make another pizza?</p>
+            <Link to="/">
+              <button>Start Again</button>
+            </Link>
+          </motion.div>
+        </>
       )}
     </AnimatePresence>
   );
