a8f85c7937ef469edc6dadd3b24d73c86fab8560
budokans <68995729+budokans@users.noreply.github.com>
2021-05-05 21:58:59 +1200

    Apply keyframes to hover scale animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,25 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
+const buttonVariants = {
+  hover: {
+    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1],
+    textShadow: "0px 0px 8px rgb(255, 255, 255)",
+    boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+  },
+};
+
 const Home = () => {
   return (
     <motion.div
       className="home container"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ delay: 0.4, duration: 1.5 }}
+      transition={{ delay: 0.6, duration: 1.5 }}
     >
       <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: "0px 0px 8px rgb(255, 255, 255)",
-            boxShadow: "0px 0px 8px rgb(255, 255, 255)",
-          }}
-        >
+        <motion.button variants={buttonVariants} whileHover="hover">
           Create Your Pizza
         </motion.button>
       </Link>
