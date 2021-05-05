23979c17e30773339d82e5090bb4d7e93d155863
budokans <68995729+budokans@users.noreply.github.com>
2021-05-05 20:37:27 +1200

    Animate Home div opacity

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="home container">
+    <motion.div
+      className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
       <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
         <button>Create Your Pizza</button>
       </Link>
-    </div>
+    </motion.div>
   );
 };
 
